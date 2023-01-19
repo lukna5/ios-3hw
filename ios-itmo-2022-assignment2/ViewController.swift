b1c1f69322722880885185881e72501c464aa2e5
Kononov Vladimir <72575440+lukna5@users.noreply.github.com>
2023-01-19 20:13:34 +0300

    Initial commit

---
+++ ios-itmo-2022-assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-itmo-2022-assignment2
+//
+//  Created by rv.aleksandrov on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
